e7c3780a1f2111bf9b231e7fb64b6a71ccadbfb1
nhloi9 <nguyenhuuloi23052001@gmail.com>
2024-02-01 17:36:29 +0700

    ldldkdk

--- src/socket.js
+++ src/socket.js
@@ -7,7 +7,7 @@ class Socket {
 	}
 	connect(token) {
 		this.disconnect();
-		this.socket = io('ws://' + backendHost, {
+		this.socket = io('wss://' + backendHost, {
 			// this.socket = io('ws://' + backendHost, {
 			withCredentials: true,
 			auth: {
